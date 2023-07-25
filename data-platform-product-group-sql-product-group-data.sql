efc4d7403f362ef306230d1a4e2ae7fad255b4b6
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-07-25 12:36:35 +0900

    release

--- data-platform-product-group-sql-product-group-data.sql
+++ data-platform-product-group-sql-product-group-data.sql
@@ -1,6 +1,11 @@
 CREATE TABLE `data_platform_product_group_product_group_data`
 (
-  `ProductGroup`              varchar(9) NOT NULL,
-    PRIMARY KEY (`ProductGroup`)
+  `ProductGroup`           varchar(9) NOT NULL,
+  `CreationDate`           date NOT NULL,
+  `LastChangeDate`         date NOT NULL,
+  `IsMarkedForDeletion`    tinyint(1) DEFAULT NULL,
+
+  PRIMARY KEY (`ProductGroup`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-product-group-sql-product-group-text-data.sql
+++ data-platform-product-group-sql-product-group-text-data.sql
@@ -2,8 +2,15 @@ CREATE TABLE `data_platform_product_group_product_group_text_data`
 (
   `ProductGroup`               varchar(9) NOT NULL,
   `Language`                   varchar(2) NOT NULL,
-  `ProductGroupName`           varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`ProductGroup`, `Language`),
-    CONSTRAINT `DataPlatformProductGroupProductGroupTextData_fk` FOREIGN KEY (`ProductGroup`) REFERENCES `data_platform_product_group_product_group_data` (`ProductGroup`)
+  `ProductGroupName`           varchar(100) NOT NULL,
+  `CreationDate`               date NOT NULL,
+  `LastChangeDate`             date NOT NULL,
+  `IsMarkedForDeletion`        tinyint(1) DEFAULT NULL,
+
+  PRIMARY KEY (`ProductGroup`, `Language`),
+
+  CONSTRAINT `DataPlatformProductGroupProductGroupTextData_fk` FOREIGN KEY (`ProductGroup`) REFERENCES `data_platform_product_group_product_group_data` (`ProductGroup`),
+  CONSTRAINT `DataPlatformPaymentMethodTextDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
